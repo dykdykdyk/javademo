e48a686778943339bb74c74d19a61b4a8e2550f1
dykdykdyk <1825041608@qq.com>
2017-03-21 11:12:45 +0800

    first command

---
+++ this/Student.java
@@ -0,0 +1,12 @@
+package com.dykthis;
+
+public class Student {
+     private String name;
+     public void setName(String name) {//设置姓名，定义一个形参name
+	       this.name=name; //将局部变量的值传递给成员变量 }
+     }
+     public String getName() {//设置姓名，定义一个形参name
+	      return this.name; //将局部变量的值传递给成员变量 }
+   }
+     
+}
---
+++ this/ThisExample01.java
@@ -0,0 +1,11 @@
+package com.dykthis;
+
+public class ThisExample01 {
+
+	public static void main(String[] args) {
+		Student stu=new Student();
+		stu.setName("ÀîÃ÷");
+		System.out.println(stu.getName());
+	}
+
+}
